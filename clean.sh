6e7a0a7d7c888caefc9d843c389d0e165c4bae23
Chattrapat Sangmanee <aomsin27@hotmail.co.th>
2026-01-12 08:50:13 +0700

    Add clean.sh

---
+++ clean.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+rm -rf BadWDSD/BadWDSD/build
+rm -rf BadWDSD/BadWDSD/.vscode
+
+rm -rf BadWDSD-Stage/.vscode
+
+rm BadWDSD-Stage/Stage0.bin
+rm BadWDSD-Stage/Stage0.elf
+
+rm BadWDSD-Stage/Stage2j.bin
+rm BadWDSD-Stage/Stage2j.elf
+
+rm BadWDSD-Stage/Stage3j.bin
+rm BadWDSD-Stage/Stage3j.elf
+
+rm BadWDSD-Stage/Stage3ja.bin
+rm BadWDSD-Stage/Stage3ja.elf
+
+rm BadWDSD-Stage/Stage3jz.bin
+rm BadWDSD-Stage/Stage3jz.elf
+
+rm BadWDSD-Stage/Stage4j.bin
+rm BadWDSD-Stage/Stage4j.elf
+
+rm BadWDSD-Stage/Stage5j.bin
+rm BadWDSD-Stage/Stage5j.elf
+
+rm BadWDSD-Stage/Stage6j.bin
+rm BadWDSD-Stage/Stage6j.elf
+
+rm BadWDSD-Stage/Stagex.bin
+rm BadWDSD-Stage/Stagex.elf
+rm BadWDSD-Stage/Stagex_aux.bin
+
+rm BadWDSD-Stage/Aux/mymetldr.elf
+rm BadWDSD-Stage/Aux/mymetldr_debug.elf
+
+rm BadWDSD-Stage/Aux/Stagex_aux.bin
+
+rm BadWDSD-Stage/Aux/Stagex_spu.elf
+rm BadWDSD-Stage/Aux/Stagex_spu_debug.elf
+
+rm -rf BadWDSD-Stage/Aux/temp
+
+rm -rf BadWDSD-SW/.vscode
+rm -rf BadWDSD-SW/build
+
+rm BadWDSD-SW/BadWDSD-SW.elf
+rm BadWDSD-SW/BadWDSD-SW.fake.self
+rm BadWDSD-SW/BadWDSD-SW.gnpdrm.pkg
+rm BadWDSD-SW/BadWDSD-SW.self
+
+rm tools/coreos_tools/coreos_tools
+
+rm tools/dtbImage_ps3_bin_to_elf/dtbImage_ps3_bin_to_elf
+rm tools/dtbImage_ps3_bin_to_elf/dtbImage.ps3.bin
+rm tools/dtbImage_ps3_bin_to_elf/dtbImage.ps3.elf
+
+rm tools/lv0gen/lv0gen
+rm tools/lv1gen/lv1gen
+rm tools/lv2gen/lv2gen
+
+rm tools/zgen/zgen
